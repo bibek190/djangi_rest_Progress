fe5c9226ddddb001e03063f1c92c91d5c42f640b
bibek190 <bibek9818917036@gmail.com>
2025-10-08 16:44:49 +1100

    add class

--- django_rest_main/api/urls.py
+++ django_rest_main/api/urls.py
@@ -1,9 +1,10 @@
 
 from django.urls import path
-from .views import studentsView,studentDetailView
+from .views import studentsView,studentDetailView,EmployeeView
 
 urlpatterns = [
     path("students/",studentsView),
     path("students/<int:pk>/",studentDetailView),
+    path("employees/",EmployeeView.as_view())
     
 ]
