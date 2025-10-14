9fb44df933d3e1fc07595a0a87ceedb3d5c60522
bibek190 <bibek9818917036@gmail.com>
2025-10-14 21:10:41 +1100

    auth

---
+++ g19/api/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Student
+from .serializers import StudentSerializers
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated,AllowAny,IsAdminUser
+
+
+# Create your views here.
+class StudentModelViewSet(viewsets.ModelViewSet):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializers
+    authentication_classes = [BasicAuthentication]
+    # permission_classes = [IsAuthenticated]
+    permission_classes = [IsAdminUser]
+    
+
+
+
+    
